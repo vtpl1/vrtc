26f0de5f87de1d43d5804aecf8ec35bb2dd0f69e
Monotosh Das <monotosh.das@videonetics.com>
2026-03-24 16:22:32 +0530

    avfto mp4 removed

--- internal/avftomp4/config.go
+++
@@ -1,6 +0,0 @@
-package avftomp4
-
-type Config struct {
-	Input  string `json:"input,omitempty"  mapstructure:"input"  yaml:"input,omitempty"`
-	Output string `json:"output,omitempty" mapstructure:"output" yaml:"output,omitempty"`
-}
